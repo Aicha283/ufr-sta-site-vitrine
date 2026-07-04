b19d6fc7e6ad918a579e6cf19b77316743fd8d05
Aicha283 <sakhoaicha@uam.edu.sn>
2026-07-04 15:13:48 +0000

    mise a jour de seed.py

--- seed.py
+++ seed.py
@@ -36,6 +36,22 @@ with app.app_context():
         debouches="Développeur, Technicien réseau, Analyste , ingenieur en iot",
         departement_id=dept_info.id
     )
+    licence_MATH = Formation(
+        nom="Licence MATHEMATIQUE",
+        niveau="Licence",
+        duree="3 ans",
+        conditions_admission="Baccalauréat série S ou équivalent",
+        debouches="",
+        departement_id=dept_math.id
+    )
+    licence_phy = Formation(
+        nom="Licence PHYSIQUE",
+        niveau="Licence",
+        duree="3 ans",
+        conditions_admission="Baccalauréat série S ou équivalent",
+        debouches="",
+        departement_id=dept_phy.id
+    )
     master_info = Formation(
         nom="Master Informatique",
         niveau="Master",
@@ -44,16 +60,52 @@ with app.app_context():
         debouches="Ingénieur logiciel, Chef de projet, Chercheur",
         departement_id=dept_info.id
     )
+    master_MATH = Formation(
+        nom="Master Informatique",
+        niveau="Master",
+        duree="2 ans",
+        conditions_admission="Licence mathematique ou  ou équivalent",
+        debouches="",
+        departement_id=dept_math.id
+    )
+    master_phy = Formation(
+        nom="Master physique",
+        niveau="Master",
+        duree="2 ans",
+        conditions_admission="Licence physique ou équivalent",
+        debouches="",
+        departement_id=dept_info.id
+    )
     db.session.add(licence_info)
+    db.session.add(licence_MATH)
+    db.session.add(licence_phy)
     db.session.add(master_info)
+    db.session.add(master_MATH)
+    db.session.add(master_phy)
     db.session.commit()
 
     # ===== MODULES =====
     db.session.add(Module(nom="Algorithmique", semestre=1, formation_id=licence_info.id))
     db.session.add(Module(nom="Python", semestre=1, formation_id=licence_info.id))
-    db.session.add(Module(nom="Mathématiques", semestre=1, formation_id=licence_info.id))
-    db.session.add(Module(nom="POO", semestre=2, formation_id=licence_info.id))
-    db.session.add(Module(nom="Base de données", semestre=2, formation_id=licence_info.id))
+    db.session.add(Module(nom="cyber securite", semestre=1, formation_id=licence_info.id))
+    db.session.add(Module(nom="POO", semestre=1, formation_id=licence_info.id))
+    db.session.add(Module(nom="Base de données", semestre=1, formation_id=licence_info.id))
+    db.session.add(Module(nom="Systeme des object connecte", semestre=2, formation_id=licence_info.id))
+    db.session.add(Module(nom="data science", semestre=2, formation_id=licence_info.id))
+    db.session.add(Module(nom="systeme resseaux", semestre=2, formation_id=licence_info.id))
+    db.session.add(Module(nom="genie logiciel", semestre=2, formation_id=licence_info.id))
+    db.session.add(Module(nom="developpement web", semestre=1, formation_id=licence_info.id))
+
+    db.session.add(Module(nom="Algebre1", semestre=1, formation_id=licence_MATH.id))
+    db.session.add(Module(nom="Analyse1", semestre=1, formation_id=licence_MATH.id))
+    db.session.add(Module(nom="Analyse numerique matricielle", semestre=1, formation_id=licence_MATH.id))
+    db.session.add(Module(nom="Algebre2", semestre=2, formation_id=licence_MATH.id))
+    db.session.add(Module(nom="Analyse2", semestre=2, formation_id=licence_MATH.id))
+
+    db.session.add(Module(nom="electricite", semestre=1, formation_id=licence_phy.id))
+    db.session.add(Module(nom="optique numerique", semestre=1, formation_id=licence_phy.id))
+    db.session.add(Module(nom="mecanique generale", semestre=2, formation_id=licence_phy.id))
+    db.session.add(Module(nom="mecanique quantique", semestre=2, formation_id=licence_phy.id))
     db.session.commit()
 
     # ===== ACTUALITÉS =====
@@ -65,7 +117,7 @@ with app.app_context():
         photo="default.jpg"
     ))
     db.session.add(Actualite(
-        titre="Soutenance de thèse — Dr. Sall",
+        titre="Soutenance de thèse — Dr. DIENG",
         date=date(2026, 6, 15),
         description="Soutenance de thèse de doctorat en informatique.",
         photo="default.jpg"
@@ -74,20 +126,52 @@ with app.app_context():
 
     # ===== ENSEIGNANTS =====
     db.session.add(Enseignant(
-        nom="Dr. Moussa Diallo",
-        grade="Maître de Conférences",
-        email="m.diallo@ufr-sta.edu",
-        domaines_recherche="Intelligence Artificielle, Machine Learning",
+        nom="Dr. Amadou Dahirou Gueye",
+        grade=" Professeur et Maître de Conférences",
+        email="dahirou.gueye@uam.edu.sn",
+        domaines_recherche="data science et java avance",
         photo="enseignant1.jpg",
         departement_id=dept_info.id
     ))
     db.session.add(Enseignant(
-        nom="Dr. Aminata Sow",
+        nom="Dr. Lamine yade",
+        grade=" Professeur ",
+        email="lameyade9@gmail.com",
+        domaines_recherche="developpement web",
+        photo="enseignant2.jpg",
+        departement_id=dept_info.id
+    ))
+    db.session.add(Enseignant(
+        nom="Dr. Sada Anne",
+        grade=" Professeur ",
+        email="sada.anne@uabd.edu.sn",
+        domaines_recherche="systeme resseaux",
+        photo="enseignant3.jpg",
+        departement_id=dept_info.id
+    ))
+    db.session.add(Enseignant(
+        nom="Dr.  Sow",
         grade="Professeur Titulaire",
         email="a.sow@ufr-sta.edu",
-        domaines_recherche="Algèbre, Analyse numérique",
-        photo="enseignant2.jpg",
+        domaines_recherche="Algèbre",
+        photo="enseignant4.jpg",
         departement_id=dept_math.id
+    ))
+    db.session.add(Enseignant(
+        nom="Dr.  coulibaly",
+        grade="Professeur Titulaire",
+        email="a.sow@ufr-sta.edu",
+        domaines_recherche=" Analyse",
+        photo="enseignant5.jpg",
+        departement_id=dept_math.id
+    ))
+    db.session.add(Enseignant(
+        nom="Dr.  Sow",
+        grade="Professeur Titulaire",
+        email="a.sow@ufr-sta.edu",
+        domaines_recherche="mecanique quantique",
+        photo="enseignant6.jpg",
+        departement_id=dept_phy.id
     ))
     db.session.commit()
 
