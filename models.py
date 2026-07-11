db659be491081b818214f8732e73dd48d55d4e75
Haady4 <hadydieng2004@gmail.com>
2026-07-11 20:10:16 +0000

    Mis à jour du site

--- models.py
+++ models.py
@@ -77,3 +77,10 @@ class Admin(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
+
+class MessageContact(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nom = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(120), nullable=False)
+    sujet = db.Column(db.String(200), nullable=False)
+    message = db.Column(db.Text, nullable=False)
